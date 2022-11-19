68f0d83f2b59cdf6ee46158a74547cd92676095b
nathan costa <nathan.lanna0801@gmail.com>
2022-11-19 18:56:18 -0300

    sendmsg

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,24 @@
+import axios from "axios";
 import icon from "../../assets/Img/notify-Icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./style.css"
-function NotificationButton() {
+
+type Props= {
+    saleId: number;
+}
+
+function NotificationButton({saleId}:Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={()=> handleClick(saleId)}>
             <img src= {icon} alt="notificar"></img>
         </div>
     )
 }
 export default NotificationButton
+
+function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+      .then(resposta => {
+        console.log("SUCESS");
+})
+}
