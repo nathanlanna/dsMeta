6ff275ea411cdfa5507e666b01ee880a29696bed
nathan costa <nathan.lanna0801@gmail.com>
2022-11-19 17:51:13 -0300

    Sale listing

--- frontend/src/components/Main/SalesCard.tsx
+++ frontend/src/components/Main/SalesCard.tsx
@@ -9,22 +9,22 @@ import { Sales } from "../../models/Sales";
 
 function SalesCard() {
 
-  const min = new Date(new Date().setDate(new Date().getDate()-365));
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
 
   const max = new Date();
 
   const [minDate, setMinDate] = useState(min);
 
   const [maxDate, setMaxDate] = useState(max);
-  
-  const [sales,setSales] = useState<Sales[]>([]);
-  useEffect (()=>{
-      axios.get(`${BASE_URL}/sales`)
-      .then(resposta=>{
-          setSales(resposta.data.content);
-          console.log(sales)
+
+  const [sales, setSales] = useState<Sales[]>([]);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales`)
+      .then(resposta => {
+        setSales(resposta.data.content);
+        console.log(resposta.data.content);
       })
-  },[]);
+  }, []);
   return (
     <div className="dsmeta-card">
       <h2 className="dsmeta-sales-title">Vendas</h2>
@@ -35,7 +35,7 @@ function SalesCard() {
             onChange={(date: Date) => setMinDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
-          /> 
+          />
         </div>
         <div className="dsmeta-form-control-container">
           <DatePicker
@@ -61,51 +61,31 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-          </tbody>
+            {sales.map(sale => {
+              return (
+                <tr key={sale.id}>
+                  <td className="show992">{sale.id}</td>
+                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td className="show992">{sale.deals}</td>
+                  <td>{`R$${sale.amount.toFixed(2)}`}</td>
+                  <td>
+                    <div className="dsmeta-red-btn-container">
+                      <NotificationButton />
+                    </div>
+                  </td>
+                </tr>
+              )
+            }
+            )
+            }
+        </tbody>
 
-        </table>
-      </div>
-
+      </table>
     </div>
+
+    </div >
   )
 }
 export default SalesCard;
--- frontend/src/models/Sales.ts
+++ frontend/src/models/Sales.ts
@@ -1,8 +1,9 @@
-export type Sales{
+export type Sales = {
 
     amount: number;
     deals : number;
     id : number;
     sellerName : String;
+    date : String;
     visited : number;
 }
